0861c476b6d14edf294235d9cad8f04cafd22009
DureSameen <dmeena_pk@yahoo.com>
2016-02-14 00:11:03 +0500

    first checkin

---
+++ HotelBedsApp/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HotelBedsApp.ViewModel;
+namespace HotelBedsApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            
+            return View(new SearchCriteriaViewModel());
+        }
+        string SetServerTime()
+        {
+            DateTime dt = System.DateTime.Now.AddDays(7);
+            return dt.Day.ToString("00") + "/" + dt.Month.ToString("00") + "/" + dt.Year;
+        }
+        string SetCurrentDateTime()
+        {
+            DateTime dt = System.DateTime.Now;
+            return dt.Day.ToString("00") + "/" + dt.Month.ToString("00") + "/" + dt.Year;
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(SearchCriteriaViewModel searchcriteria)
+        {
+            //When user enter search criteria, page will be validated by its model class data annotations, like required, datatype etc.
+            //If all data is valid  , it will put it into Session and take user to wait page.
+            if (ModelState.IsValid)
+            {
+                Session["SearchCriteria"] = searchcriteria;
+
+
+                return RedirectToAction("Index", "result");
+            }
+
+            else
+            {   //if page is not validated then it will remain on same view.
+                ViewBag.ServerDatetime = SetServerTime();
+                ViewBag.CurrentDatetime = SetCurrentDateTime();
+                return View(searchcriteria);
+            }
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ hotel-api-sdk/types/HotelApiVersion.cs
@@ -0,0 +1,14 @@
+﻿namespace com.hotelbeds.distribution.hotel_api_sdk.types
+{
+    public class HotelApiVersion
+    {
+        public enum versions { V0_2, V1 };
+
+        public versions version { get; set; }
+
+        public HotelApiVersion(versions version)
+        {
+            this.version = version;
+        }
+    }
+}
